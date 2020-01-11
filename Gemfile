d1b4f516836fc595959052a8c217aaa39834ad19
link-marie <rijkmarie@gmail.com>
2020-01-11 10:25:48 +0900

    Implement basic login

--- Gemfile
+++ Gemfile
@@ -39,10 +39,11 @@ gem 'jbuilder', '~> 2.5'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
-
+gem 'bootstrap-sass', '3.4.1'
 gem 'activerecord-nulldb-adapter'
 
-gem 'bootstrap-sass', '3.4.1'
+gem 'jquery-rails'
+gem 'jquery-ui-rails'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,24 +2,24 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
-  # 無効なデータを与えても登録ユーザ数が変わらないことを確認する 
+  # 無効なデータを与えても登録ユーザ数が変わらないことを確認する
   test "invalid signup information" do
-    # ユーザー登録ページにアクセス
+  # ユーザー登録ページにアクセス
     get signup_path
-    # それぞれ無効な値でPOSTリクエストを送信     
+    # それぞれ無効な値でPOSTリクエストを送信
     assert_no_difference 'User.count' do
       post users_path, params: { user: { name:  "",
                                          email: "user@invalid",
                                          password:              "foo",
                                          password_confirmation: "bar" } }
     end
-    # POSTが失敗すると new actionが再描画される   
+    # POSTが失敗すると new actionが再描画される
     assert_template 'users/new'
     assert_select 'form[action="/signup"]'
     assert_select 'div#error_explanation'
     assert_select 'div.field_with_errors'
   end
-  
+
   test "valid signup information" do
     get signup_path
     assert_difference 'User.count', 1 do
@@ -31,6 +31,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     follow_redirect!
     assert_template 'users/show'
     assert_not flash.empty?
+    assert is_logged_in?
   end
 
 end
